a4fffd6456c36d16f5442cf67579efc34ea6deea
Albert Kolberg <albert@kolberg.com.br>
2022-04-20 18:18:17 -0300

    Set to depend on our own Spectre fork

--- Package@swift-4.2.swift
+++ Package@swift-4.2.swift
@@ -7,7 +7,7 @@ let package = Package(
     .library(name: "PathKit", targets: ["PathKit"]),
   ],
   dependencies: [
-    .package(url:"https://github.com/kylef/Spectre.git", .upToNextMinor(from:"0.9.0"))
+    .package(url:"git@github.com:warren-dev-ios/Spectre.git", .branch("master"))
   ],
   targets: [
     .target(name: "PathKit", dependencies: [], path: "Sources"),
